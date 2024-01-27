a62f10e5f4a43bec9f56335a68364269f6d3fd2a
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-01-27 17:55:47 +0600

    Delete routes directory

--- routes/dashboard.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express();
-const { Index } = require("../controllers/dashboard.controller");
-const CheckLogin = require("../middlewares/checkLogin");
-
-router.get("/dashboard", CheckLogin, dashboard);
-
-module.exports = router;
