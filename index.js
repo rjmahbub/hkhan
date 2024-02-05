a085dfa2abb147be2fd8b7e841782b304e02de02
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-02-05 10:51:57 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-return 'hello';
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+require('dotenv').config();
+const userRouter = require("./routes/users.route");
+const dashboardRouter = require("./routes/dashboard.route");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(userRouter);
+app.use(dashboardRouter);
+
+//database connection with mongoose
+const connectionpOptions = {dbName: `hkhan`}
+mongoose.connect(process.env.DB_CONNECT_URL, connectionpOptions)
+.then(() => {
+  const PORT = 3000;
+  app.listen(PORT, ()=>{
+    console.log(`app listening http://localhost:${PORT}`);
+  });
+  console.log('connection successfull')
+})
+.catch((err) => console.log(err));
