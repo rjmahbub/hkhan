e14e6d99e5c9a0d7119581633586a8b83305a8bb
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-02-23 21:25:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
 require('dotenv').config();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 
