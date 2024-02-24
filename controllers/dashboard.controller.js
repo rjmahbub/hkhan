6b95d4ab7aa9961f249cecb65e700437d4eee19e
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-02-23 22:08:32 -0800

    Update dashboard.controller.js

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const UserModel = require('../models/users.model');
 
 exports.MealStatus = async (req, res) => {
@@ -9,4 +11,21 @@ exports.MealStatus = async (req, res) => {
   const user = await query.findOne();
    
   return res.send(userId)
+};
+
+exports.userRegister = async function (req, res) {
+  const {room, name, phone, password} = req.body;
+  const User = new UserModel({ room, name, phone, password: await bcrypt.hash(password, 10) });
+  const savedUser = await User.save();
+  if(savedUser === User){
+    return res.status(200).json({
+      success: true,
+      'message': 'Member was saved successfully!'
+    });
+  }else{
+    return res.status(500).json({
+      success: false,
+      'message': 'Server Error!'
+    });
+  }
 };
