4c3073e702b877bdb0496c0cf3ed148a4bfbf7bc
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-02-23 21:34:40 -0800

    Update dashboard.route.js

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express();
 const { MealStatus } = require("../controllers/dashboard.controller");
-const CheckLogin = require("../middlewares/AuthMiddleware");
 
 router.post("/meal-status", MealStatus);
 
