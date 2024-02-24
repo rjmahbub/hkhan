b568579fa67c6d3bf0356c015b44bc9a87391e5c
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-02-23 22:27:49 -0800

    Delete routes/dashboard.route.js

--- routes/dashboard.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express();
-const { MealStatus, userRegister } = require("../controllers/dashboard.controller");
-
-router.post("/meal-status", MealStatus);
-
-module.exports = router;
