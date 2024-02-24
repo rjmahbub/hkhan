08d0c4c56f5767f60e0bb41c520e503914e0f7be
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-02-23 21:43:30 -0800

    Update dashboard.route.js

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -3,5 +3,9 @@ const router = express();
 const { MealStatus } = require("../controllers/dashboard.controller");
 
 router.post("/meal-status", MealStatus);
+router.post("/user-register", userRegister);
+router.post("/user-login", userLogin);
+router.post("/all-users", allUsers);
+router.post('/user-update', updateUser)
 
 module.exports = router;
