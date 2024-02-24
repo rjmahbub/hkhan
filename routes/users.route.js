f1f40b50bc0d3a9d21a379ebbfdead0797bc683d
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-02-23 21:40:21 -0800

    Update users.route.js

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express();
-const { Index, userRegister, userLogin, allUsers, updateUser} = require("../controllers/users.controller");
+const { userRegister, userLogin, allUsers, updateUser} = require("../controllers/users.controller");
 const AuthMiddleware = require("../middlewares/AuthMiddleware");
 
-router.get("/verify-token", AuthMiddleware, Index);
 router.post("/user-register", userRegister);
 router.post("/user-login", userLogin);
 router.post("/all-users", allUsers);
