5adadc614c10953f4cc3126c9b71a7fed19e7832
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-02-23 21:44:58 -0800

    Update dashboard.route.js

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -7,6 +7,6 @@ router.post("/meal-status", MealStatus);
 router.post("/user-register", userRegister);
 router.post("/user-login", userLogin);
 router.post("/all-users", allUsers);
-router.post('/user-update', updateUser)
+router.post('/user-update', updateUser);
 
 module.exports = router;
