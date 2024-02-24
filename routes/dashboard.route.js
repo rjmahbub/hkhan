59f84f3ed783067a758b83b3db80a0f2d7c0d115
Mahbub Alam <81683660+rjmahbub@users.noreply.github.com>
2024-02-23 21:28:54 -0800

    Update dashboard.route.js

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const router = express();
+const { Index, checkLogin, userRegister, userLogin, allUsers, updateUser} = require("../controllers/users.controller");
+const AuthMiddleware = require("../middlewares/AuthMiddleware");
 
 module.exports = router;
